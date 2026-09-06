415f5b984d05d80c0a151c75fc4600ddab10b495
WendyGTHB <nahpavila@gmail.com>
2026-09-06 17:23:56 +0000

    Afegir pantalla d'inici amb botó Start (Tasca 3)

--- js/scenes/StartScene.js
+++ js/scenes/StartScene.js
@@ -1,13 +1,25 @@
-// Escena de inicio (aún vacía; el diseño y el botón Start llegan en la Tarea 3).
+// Pantalla de inicio con título y botón Start (Tarea 3).
 class StartScene extends Phaser.Scene {
 	constructor() {
 		super('StartScene');
 	}
 
 	create() {
-		this.add.text(640, 360, 'Start Scene', {
-			fontSize: '48px',
+		this.add.text(640, 240, 'Whack-a-Olorda', {
+			fontSize: '64px',
 			color: '#ffffff',
 		}).setOrigin(0.5);
+
+		const startButton = this.add.text(640, 420, 'Start', {
+			fontSize: '40px',
+			color: '#ffffff',
+			backgroundColor: '#2e7d32',
+			padding: { x: 24, y: 12 },
+		}).setOrigin(0.5);
+
+		startButton.setInteractive({ useHandCursor: true });
+		startButton.on('pointerdown', () => {
+			this.scene.start('GameScene');
+		});
 	}
 }
